038cbfca12643c9a3a7cef63f6aa0f6da94c3f1e
sitaratoon <ajay393798@gmail.com>
2025-01-26 20:04:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 #Please do not sell or remove credits.
 import os
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "18946488")
+API_HASH = os.getenv("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "ST_Rename_Update") # Channel Username Without @
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-100")) #Channel Id
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002320080278")) #Channel Id
 
 ENABLE_FLOOD_WAIT = bool(os.getenv("ENABLE_FLOOD_WAIT", False)) # Set "True" For Enable Floodwait
 FLOOD_WAIT_TIME = int(os.getenv("FLOOD_WAIT_TIME", 1)) #5min
