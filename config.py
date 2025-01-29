ecec7bd7130a406d39c85fd2f6578f5fdb65bbc1
sitaratoon <ajay393798@gmail.com>
2025-01-29 22:43:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "ST_Rename_Update") # Channel Usern
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002320080278")) #Channel Id
 
 ENABLE_FLOOD_WAIT = bool(os.getenv("ENABLE_FLOOD_WAIT", True)) # Set "True" For Enable Floodwait
-FLOOD_WAIT_TIME = int(os.getenv("FLOOD_WAIT_TIME", 5)) #5min
+FLOOD_WAIT_TIME = int(os.getenv("FLOOD_WAIT_TIME", 10)) #5min
