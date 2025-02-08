416af8a7221e93325f5b3d19a16f10203a47961f
sitaratoon <ajay393798@gmail.com>
2025-02-08 11:58:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@
 
 import os
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "18946488")
+API_HASH = os.getenv("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN = int(os.getenv("ADMIN", ""))
+ADMIN = int(os.getenv("ADMIN", "6692613520"))
 
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002320080278")) #Channel Id
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002320080278"))
 
-DB_URI = os.getenv("DB_URI", "")
-DB_NAME = os.getenv("DB_NAME", "")
+DB_URI = os.getenv("DB_URI", "mongodb+srv://Renamest:Renamest@cluster0.prfhc.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.getenv("DB_NAME", "Ajay")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Force Subscribe Enable
 AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002355394644") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
 ENABLE_FLOOD_WAIT = bool(os.getenv("ENABLE_FLOOD_WAIT", False)) # Set "True" For Enable Floodwait
-FLOOD_WAIT_TIME = int(os.getenv("FLOOD_WAIT_TIME", 300)) #5min
+FLOOD_WAIT_TIME = int(os.getenv("FLOOD_WAIT_TIME", 15)) #5min
